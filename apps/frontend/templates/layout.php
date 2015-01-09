403b67dd84925e7e50d2d5af2bd3b61f6754f988
sandromisic <sandro.misic@morescreens.tv>
2015-01-09 14:06:23 +0100

    gotov 19. dan

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -7,7 +7,8 @@
     <link rel="shortcut icon" href="/favicon.ico" />
       <link rel="alternate" type="application/atom+xml" title="Latest Jobs"
             href="<?php echo url_for('job', array('sf_format' => 'atom'), true) ?>" />
-    <?php include_javascripts() ?>
+      <?php use_javascript('jquery-1.4.2.min.js') ?>
+      <?php use_javascript('search.js') ?>
     <?php include_stylesheets() ?>
   </head>
   <body>
@@ -25,18 +26,18 @@
                 <a href="<?php echo url_for('job/new') ?>">Post a Job</a>
               </div>
             </div>
- 
-            <div class="search">
-              <h2>Ask for a job</h2>
-              <form action="" method="get">
-                <input type="text" name="keywords"
-                  id="search_keywords" />
-                <input type="submit" value="search" />
-                <div class="help">
-                  Enter some keywords (city, country, position, ...)
-                </div>
-              </form>
-            </div>
+
+              <div class="search">
+                  <h2>Ask for a job</h2>
+                  <form action="<?php echo url_for('job_search') ?>" method="get">
+                      <input type="text" name="query" value="<?php echo $sf_request->getParameter('query') ?>" id="search_keywords" />
+                      <input type="submit" value="search" />
+                      <img id="loader" src="/legacy/images/loader.gif" style="vertical-align: middle; display: none" />
+                      <div class="help">
+                          Enter some keywords (city, country, position, ...)
+                      </div>
+                  </form>
+              </div>
           </div>
         </div>
       </div>
@@ -69,27 +70,32 @@
           <?php echo $sf_content ?>
         </div>
       </div>
- 
-      <div id="footer">
-        <div class="content">
-          <span class="symfony">
-            <img src="/legacy/images/jobeet-mini.png" />
-            powered by <a href="/">
-            <img src="/legacy/images/symfony.gif" alt="symfony framework" />
-            </a>
-          </span>
-          <ul>
-            <li><a href="">About Jobeet</a></li>
-            <li class="feed">
-                <a href="<?php echo url_for('job', array('sf_format' => 'atom')) ?>">Full feed</a>
-            </li>
-            <li><a href="">Jobeet API</a></li>
-              <li class="last">
-                  <a href="<?php echo url_for('affiliate_new') ?>">Become an affiliate</a>
-              </li>
-          </ul>
+
+        <div id="footer">
+            <div class="content">
+    <span class="symfony">
+      <img src="/legacy/images/jobeet-mini.png" />
+      powered by <a href="/">
+            <img src="/legacy/images/symfony.gif" alt="symfony framework" /></a>
+    </span>
+                <ul>
+                    <li>
+                        <a href=""><?php echo __('About Jobeet') ?></a>
+                    </li>
+                    <li class="feed">
+                        <?php echo link_to(__('Full feed'), 'job', array('sf_format' => 'atom')) ?>
+                    </li>
+                    <li>
+                        <a href=""><?php echo __('Jobeet API') ?></a>
+                    </li>
+                    <li class="last">
+                        <?php echo link_to(__('Become an affiliate'), 'affiliate_new') ?>
+                    </li>
+                </ul>
+                <?php include_component('language', 'language') ?>
+            </div>
         </div>
-      </div>
     </div>
+    <?php include_javascripts() ?>
   </body>
 </html>
